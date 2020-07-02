f57a79a1a5566e7947f6382ba5c24588410c2547
Matt Watson <matt@mwatson.co.uk>
2020-07-02 21:45:23 +0100

    #12 - Email Sudo Code

--- inc/email-notifications/namespace.php
+++ inc/email-notifications/namespace.php
@@ -32,17 +32,24 @@ function setup() : void {
 
 function handle_emails( $post, $request, $creating = true ) {
 
-	// Code to save post authors here: META_KEY_POST_CONTRIBUTORS.
-	//error_log( get_current_user_id() );
+	// If any user edits the post, log their user ID.
+	$current_user_id   = (int) get_current_user_id();
+	$post_contributors = (array) get_post_meta( $post->ID, META_KEY_POST_CONTRIBUTORS, true );
+	if ( ! in_array( $current_user_id, $post_contributors, true ) ) {
+		$post_contributors[] = $current_user_id;
+		update_post_meta( $post->ID, META_KEY_POST_CONTRIBUTORS, $post_contributors );
+	}
 
 	if ( ! isset( $request['meta'] ) || ! isset( $request['meta'][ META_KEY_BLOCK_COMMENTS ] ) ) {
 		return;
 	}
 
-	$comments     = (array) $request['meta'][ META_KEY_BLOCK_COMMENTS ];
-	$comments_old = (array) get_post_meta( $post->ID, META_KEY_BLOCK_COMMENTS_PREVIOUS, true );
-	$comments_new = [];
+	$comments          = (array) $request['meta'][ META_KEY_BLOCK_COMMENTS ];
+	$comments_old      = (array) get_post_meta( $post->ID, META_KEY_BLOCK_COMMENTS_PREVIOUS, true );
+	$comments_new      = [];
+	$contacted_authors = [];
 
+	// Get the new comments.
 	if ( count( $comments_old ) < 1 || empty( $comments_old[0] ) ) {
 		$comments_new = $comments;
 	} else {
@@ -55,37 +62,63 @@ function handle_emails( $post, $request, $creating = true ) {
 		);
 	}
 
+	// When we do the check, we don't want the original author emailing.
+	if ( ! in_array( (int) $post->post_author, $contacted_authors, true ) ) {
+		$contacted_authors[] = (int) $post->post_author;
+	}
+
 	foreach ( $comments_new as $comment ) {
+
+		// When we do the check, we don't want the comment author emailing.
+		if ( ! in_array( (int) $comment['authorID'], $contacted_authors, true ) ) {
+			$contacted_authors[] = (int) $comment['authorID'];
+		}
 
 		// Comment is new, email original author.
 		if ( 0 === (int) $comment['parent'] ) {
 			// Ensure comment author is not original author.
 			if ( (int) $post->post_author !== (int) $comment['authorID'] ) {
 				// Email original author: username has left a comment on a post you authored.
-				error_log( $post->post_author . ', ' . $comment['authorID'] . 'has left a comment on a post you authored.' );
+				send_email(
+					$post->ID,
+					$post->post_author,
+					$comment['authorID'],
+					esc_html_e( '[username] has commented on a post that you authored.', 'wholesome-publishing' ),
+					$comments_new
+				);
 			}
 		}
 
-		// TODO: Save all post authors using: and notify them "a post you contributed to"
-		// get_post_meta( $post->ID, '_edit_last', true ); // THIS DOES NOT WORK, use get_current_user_id();
-		// META_KEY_POST_CONTRIBUTORS
-
+		// Comment is reply.
 		if ( 0 !== (int) $comment['parent'] ) {
 
 			// Email original author.
 			// Ensure comment author is not original author.
 			if ( (int) $post->post_author !== (int) $comment['authorID'] ) {
 				// Email original author: username has replied to a comment on a post you authored.
-				error_log( $post->post_author . ', ' . $comment['authorID'] . 'has replied to a comment on a post you authored.' );
+				send_email(
+					$post->ID,
+					$post->post_author,
+					$comment['authorID'],
+					esc_html_e( '[username] has replied to a comment that you authored.', 'wholesome-publishing' ),
+					$comments_new
+				);
 			}
 
-			// Email comment author.
+			// Email comment parent author.
 			$key            = array_search( $comment['parent'], array_column( $comments, 'dateTime' ) );
-			$comment_parent = $comment[ $key ];
-			// Ensure comment author is not original comment author.
-			if ( $comment_parent['authorID'] !== $comment['authorID'] ) {
+			$comment_parent = $comments[ $key ];
+			// Ensure comment author is not original comment author, and is not the parent author (already mailed, or it is them)
+			if ( $comment_parent['authorID'] !== $comment['authorID'] && (int) $comment_parent['authorID'] !== (int) $post->post_author ) {
 				// Email parent comment author: username has replied to your comment.
-				error_log( $comment_parent['authorID'] . ', ' . $comment['authorID'] . 'has replied to your comment.' );
+				send_email(
+					$post->ID,
+					$comment_parent['authorID'],
+					$comment['authorID'],
+					esc_html_e( '[username] has replied to your comment.', 'wholesome-publishing' ),
+					$comments_new
+				);
+				$contacted_authors[] = (int) $comment_parent['authorID'];
 			}
 
 			// Email thead commentators.
@@ -105,16 +138,59 @@ function handle_emails( $post, $request, $creating = true ) {
 					continue;
 				}
 
-				// Comment is by current author, bail.
+				// Comment is by comment author, bail.
 				if ( $comment_child['authorID'] === $comment_parent['authorID'] ) {
 					continue;
 				}
 
+				// User already emailed, bail.
+				if ( in_array( $comment_parent['authorID'], $contacted_authors, true ) ) {
+					continue;
+				}
+
 				// Email thread contributor: username has replied to a comment you have replied to.
-				error_log( $comment_parent['authorID'] . ', ' . $comment['authorID'] . 'has replied to a comment you have replied to.' );
+				send_email(
+					$post->ID,
+					$comment_child['authorID'],
+					$comment['authorID'],
+					esc_html_e( '[username] has replied to a comment that you also replied to.', 'wholesome-publishing' ),
+					$comments_new
+				);
+				$contacted_authors[] = (int) $comment_parent['authorID'];
+			}
+		}
+
+		// Email post contributors.
+		foreach ( $post_contributors as $post_contributor_id ) {
+			if ( ! in_array( $post_contributor_id, $contacted_authors, true ) ) {
+				// Email post contributor: username has replied to a post you have contributed to.
+				send_email(
+					$post->ID,
+					$post_contributor_id,
+					$comment['authorID'],
+					esc_html_e( '[username] has commented on a post that you have contributed to.', 'wholesome-publishing' ),
+					$comments_new
+				);
 			}
 		}
 	}
 
 	update_post_meta( $post->ID, META_KEY_BLOCK_COMMENTS_PREVIOUS, $comments );
+}
+
+function send_email( $post_id, $recipient_id, $commenter_id, $message, $comments ) {
+
+	$message = str_replace( '[username]', 'ACTUAL USERNAME', $message );
+
+	$body  = '<p>' . $message . '</p>';
+	$body .= ''; // Loop $comments and display as ul/li [username]:[comment] (view). // TODO, query param that will open the comment.
+	$body .= ''; // View the comments // TODO, query param that will open the sidebar.
+	$body .= ''; // View the post.
+	$body .= ''; // To opt out of these emails check your settings. // TODO: email frequency user settings page.
+
+	$to      = // ACTUAL EMAIL ADDRESS ;
+	$subject = esc_html( $message );
+	$headers = [ 'Content-Type: text/html; charset=UTF-8' ];
+
+	wp_mail( $to, $subject, $body, $headers );
 }
